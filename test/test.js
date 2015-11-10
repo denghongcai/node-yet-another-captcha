167d66bab00b900051f242f1ed1176539a9fc04f
Hongcai Deng <admin@dhchouse.com>
2015-11-10 18:42:36 +0800

    add verify test

--- test/test.js
+++ test/test.js
@@ -10,7 +10,10 @@ let fs = require('fs');
 let captcha = new Captcha();
 
 captcha.generate().then(captcha => {
-    console.log(captcha.answer.toString());
+    let answer = captcha.answer.toString();
+    if(captcha.verify(answer)) {
+        console.log('guess right!');
+    }
     fs.writeFile('test.gif', captcha.question());
 });
 
